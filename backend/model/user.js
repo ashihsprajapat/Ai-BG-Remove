0c5ac79bb28decbb6277ab9c4d7b58bca2c233e6
ashish prajapat <ashishprajapat507@gmail.com>
2025-09-18 11:37:51 +0530

    remove required keyword in  user mode in firstName and lastName property

--- backend/model/user.js
+++ backend/model/user.js
@@ -4,8 +4,8 @@ import mongoose, { model, Schema } from "mongoose";
 const userSchema = new Schema({
     clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true },
-    firstName: { type: String, required: true },
-    LastName: { type: String, required: true },
+    firstName: { type: String, },
+    LastName: { type: String, },
     creditBalanace: { type: Number, default: 5 },
     photo: { type: String, required:true }
 })
