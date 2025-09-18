8f311a6c4cac2874e29924612152a6708cf7e970
ashish prajapat <ashishprajapat507@gmail.com>
2025-09-18 09:40:22 +0530

    set up data connectivity

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -5,8 +5,8 @@ import 'dotenv/config'
 
 
 const connectToDB = async () => {
-    mongoose.connection.on('connected', ()=>{
-        console.log("Connect to database")
+    mongoose.connection.on('connected', () => {
+        console.log("Connect to database Ai bg remove")
     })
     await mongoose.connect(`${process.env.DB_URL}/AI_Bg_Remove`);
 }
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,62 +1,9 @@
 
 
-import { User } from './../model/user.model.js';
-import bcrypt from 'bcrypt';
-import crypto from "crypto"
-import bodyParser from 'body-parser';
+//api controller function to mangae clerk user with database
+//api/user/webhooks
 
-export const Login = async (req, res) => {
-    let { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ message: "username or passworder  required" })
-    }
-    let user = await User.findOne({ username });
-    if (!user) {
-        return res.status(400).json({ message: "Username not exits" })
-    }
-    let match = await bcrypt.compare(password, user.password);
-    if (!match) {
-        return res.status(400).json({ message: "Wrong password" })
-    }
-    let token = await crypto.randomBytes(20).toString("hex");
-    user.token = token;
-    await user.save();
-    return res.status(200).json({ token })
+const webhooks = async (req, res) => {
+
+
 }
-
-
-export const register = async (req, res) => {
-    const { username, password, name } = req.body;
-    if (!name || !username || !password) {
-        return res.status(400).json({ message: "username or passworder  required" })
-    }
-
-    try {
-        const user = await User.findOne({ username });
-        if (user) {
-            return res.status(400).json({ message: "username already exist" });
-        }
-
-        let hashPassword = await bcrypt.hash(password, 10);
-
-        const newUser = new User({
-            name: name,
-            password: hashPassword,
-            username: username,
-        })
-
-        await newUser.save()
-            .then(() => {
-                console.log(user)
-            })
-
-        return res.status(200).json({ message: "register successfull" })
-
-
-    } catch (err) {
-        throw err;
-    }
-}
-
-
-
